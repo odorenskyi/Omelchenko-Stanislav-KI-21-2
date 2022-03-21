8deb0e1e0ead859bb78136fbb7ff8cced278fc0a
Omelchenko Stanislav <98589561+OmelchenkoStas@users.noreply.github.com>
2022-03-21 11:42:13 +0300

    Update main.cpp

--- lab08/prj/TestDriver/main.cpp
+++ lab08/prj/TestDriver/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-#include "ModulesOmelchenko.h"
+#include "Modules_Ivanchenko.h"
 
 using namespace std;
 
@@ -8,23 +8,23 @@ int main()
 {
    system("chcp 1251 && cls");
 
-    float possibleresult[5] = {1.1438 , -55767.2244 , -75.9464 , -23.3496 , -7.9294};
+    float possibleresult[5] = {-22.6132 , -22.3023 , -9.2558 , -21.0049 , -10.6245};
 
-    float x[5] = {3,81,4,6,3};
-    float y[5] = {6,2,89,4,3};
-    float z[5] = {9,6,1,94,3};
+    float x[5] = {4,4,3,6,3};
+    float y[5] = {7,8,6,3,2};
+    float z[5] = {2,2,1,94,1};
 
     for (short int i = 0; i < 5; i++)
     {
-        if (round (s_calculation(x[i], y[i], z[i])*10000/10000.0 == possibleresult[i]))
-        {
-            cout << "Test ¹[" << i + 1 << "]: passed\n";
+         cout << round(s_calculation(x[i], y[i], z[i])10000)/10000.0 << endl;
+         if (round(s_calculation(x[i], y[i], z[i])10000)/10000.0 == round(possibleresult[i]*10000)/10000.0)
+            {
+            cout << "TEST N[" << i + 1 << "]: PASSED\n";
         }
         else
         {
-            cout << "Test ¹[" << i + 1 << "]: failed\n";
+            cout << "TEST N[" << i + 1 << "]: FAILED\n";
         }
     }
 
 }
-
